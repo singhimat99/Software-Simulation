0d61dadcc8534d0c0911499ede402a825f01835b
Himat Singh <105682846+singhimat99@users.noreply.github.com>
2024-06-20 20:18:20 -0700

    Add files via upload

---
+++ story_content/5ZKkVcKmYt7_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.86.32080.0%0D%0A%0D%0A00:00:00.128%20--%3E%2000:00:04.530%0D%0AClick%20the%20%E2%80%9CBegin%E2%80%9D%20button%20below%20to%20complete%20this%20simulation!%0D%0A%0D%0A"}')
---
+++ story_content/5tZYjuXnIhV_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.86.32080.0%0D%0A%0D%0A00:00:00.112%20--%3E%2000:00:05.620%0D%0AGreat%20Job!%20You%20just%20learned%20how%20to%20create%20a%20trigger%20in%20articulate%20storyline!%0D%0A%0D%0A"}')
